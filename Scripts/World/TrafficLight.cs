b8decbac82d7b36330d2cacd806a160ae3c80828
Jamie Bailey <psndanjam123@gmail.com>
2023-04-07 17:53:26 +0100

    Traffic Lights init

---
+++ Scripts/World/TrafficLight.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonNitro.World
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class TrafficLight : UdonSharpBehaviour
+    {
+        [SerializeField, UdonSynced]
+        bool m_green = false;
+
+        [SerializeField]
+        Animator m_animator;
+
+        void Start()
+        {
+            UpdateAnimator();
+        }
+
+        public bool IsGreen()
+        {
+            return m_green;
+        }
+
+        public void SetGreen()
+        {
+            SetOwner();
+            m_green = true;
+            RequestSerialization();
+            UpdateAnimator();
+        }
+
+        public void SetRed()
+        {
+            SetOwner();
+            m_green = false;
+            RequestSerialization();
+            UpdateAnimator();
+        }
+
+        public override void OnDeserialization()
+        {
+            UpdateAnimator();
+        }
+
+        void UpdateAnimator()
+        {
+            m_animator.SetBool("Green", m_green);
+        }
+
+        bool IsOwner()
+        {
+            return Networking.IsOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        void SetOwner()
+        {
+            if (IsOwner())
+            {
+                return;
+            }
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+    }
+
+}
