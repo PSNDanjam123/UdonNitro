6f0f61a8664dda66008d0682d78eab413015c04b
Jamie Bailey <psndanjam123@gmail.com>
2023-04-08 00:25:24 +0100

    increased world size, dropped quest support

---
+++ Scripts/World/StreetLight.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace UdonNitro.World
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class StreetLight : UdonSharpBehaviour
+    {
+        VRCPlayerApi m_localPlayer;
+
+        [SerializeField]
+        Light m_light;
+        [SerializeField]
+        MeshRenderer m_lightMeshRenderer;
+
+        void Start()
+        {
+            m_localPlayer = Networking.LocalPlayer;
+        }
+
+        void FixedUpdate()
+        {
+            if (Vector3.Distance(m_localPlayer.GetPosition(), gameObject.transform.position) > 50f)
+            {
+                m_light.enabled = false;
+            }
+            else
+            {
+                m_light.enabled = true;
+            }
+        }
+    }
+
+}
